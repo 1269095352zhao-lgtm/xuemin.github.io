fb03611b886a1d080c67b5e6eb0950a807b66da7
1269095352zhao-lgtm <1269095352zhao@gmail.com>
2026-01-21 23:08:56 +0800

    Site updated: 2026-01-21 23:08:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/14/这是一个测试/"];function toRandomPost(){
+var posts=["posts/12874/","posts/1b1497bb/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
